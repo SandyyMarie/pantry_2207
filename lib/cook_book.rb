7e5297bdb81350cf1a8f6e9528ca5fc438c2f916
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-08-02 14:33:26 -0600

    refactor + fixes: after review session with altenrative fixes

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -18,5 +18,32 @@ class CookBook
             end
         end
         comparison_recipe
+
+        ##########################
+        # @recipes.max_by do |recipe| 
+        #     recipe.total_calories
+        # end
+        ##########################
+
     end
+
+    # def ingredients
+        ##########################
+        # ingredients = @recipes.map do |recipe|
+        #     recipe.ingredients_required.map do |ingredient|
+        #         ingredients[0].name
+        #     end
+        # end
+        # ingredients.flatten.uniq
+        ##########################
+        # @recipes.flat_map do |recipe|
+        #     recipe.ingredients.map do |ingredient|
+        #         ingredient.name
+        #     end
+        # end.uniq
+    # end
+
+    # @recipes.flat_map do |recipe|
+    #     recipe.ingredients.map(&:name)
+    # end.uniq
 end
